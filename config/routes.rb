b6b70f80e9075aa2628a82eeb4e2427f8a49d817
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-09-09 16:49:43 -0300

    index client

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+
   namespace :api do
     namespace :v1 do
+      devise_for :users
       resources :tickets
       resources :clients
       resources :mobile_devices
       root to: "home#index"
       get 'search_clients', to: 'clients#search', as: 'search_clients'
-
       resources :tickets do
         collection do
           get 'status/:status', to: 'tickets#status', as: :status
