0f30ed43dac8a94eaeef2d258eecb4f0aad68b56
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-10-08 22:35:18 -0300

    status add

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,10 +2,10 @@ class Ticket < ApplicationRecord
   belongs_to :mobile_device
   belongs_to :user
 
-  enum status: { "Pendente" => 0, "Em andamento" => 1, "Reparo concluído" => 2, "Pedido entregue" => 3 }
+  enum status: { "Pendente" => 0, "Em andamento" => 1, "Peça em transito" => 2, "Reparo concluído" => 3, "Pedido entregue" => 4 }
   enum pecas: { "Bateria" => 0, "Tela" => 1, "Placa Sub" => 2 }
   enum sintoma: { "Energia" => 0, "Software" => 1, "Hardware" => 2 }
 
-  validates :data_abertura, :data_fechamento, :descricao, :status, :sintoma, presence: true
+  validates :descricao, :status, :sintoma, presence: true
   validates :mobile_device_id, uniqueness: { scope: [:status], message: "Ticket already exists for this device with the same status." }
 end
