83407a7f1b181191973b9a8483a53f2f4f8bedea
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-11-26 23:44:54 -0300

    cors render

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  %r{\Ahttps://techassist\.[a-z]+\z}
+    origins 'https://techassist-ggaa.onrender.com'
     resource '/api/*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
