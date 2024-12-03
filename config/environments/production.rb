fd466747507c36d927c683a1f2e2caf871cfdec3
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-12-02 22:11:18 -0300

    Fix error on render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.hosts << "techassist-ggaa.onrender.com"
+  config.hosts << "assisttech-web.onrender.com"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
