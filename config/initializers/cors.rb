8d4d034bc192c6db08fdefa1350db46e4776d745
lucasleandro1 <lukkassoliveira2215@gmail.com>
2024-12-03 20:27:29 -0300

    cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://assisttech-web.onrender.com/', 'http://localhost:3001' , 'https://frontend-web-five-sigma.vercel.app/'
+    origins 'https://assisttech-web.onrender.com', 'http://localhost:3001' , 'https://frontend-web-five-sigma.vercel.app'
     resource '/api/*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head],
